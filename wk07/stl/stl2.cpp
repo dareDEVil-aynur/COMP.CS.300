5c098a47020eaeb00bae6885092c6ee4ee527103
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2023-10-22 19:35:49 +0300

    sort desc

--- wk07/stl/stl2.cpp
+++ wk07/stl/stl2.cpp
@@ -6,15 +6,21 @@
 
 using namespace std;
 
-
 /**
- * @brief sort a given vector to an descending order
+ * @brief sort a given vector to a descending order
  *
  * @param v a vector to be sorted
  * @return int EXIT_SUCCESS if everything went OK, EXIT_FAILURE otherwise
  */
 int sortDesc(std::vector<int>& v)
 {
-    return EXIT_FAILURE;
+    try 
+    {
+        std::sort(v.begin(), v.end(), [](int a, int b) { return a > b; }); // Sorting the vector in descending order using a lambda function
+        return EXIT_SUCCESS;
+    } 
+    catch(...) 
+    {
+        return EXIT_FAILURE;
+    }
 }
-
