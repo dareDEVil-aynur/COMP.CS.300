6729bf167fc0c2b5b56da800a9dd57941a4bea61
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2023-10-29 19:19:59 +0200

    q2

--- wk08/iterator_invalidation/invalidation2.cc
+++ wk08/iterator_invalidation/invalidation2.cc
@@ -11,9 +11,12 @@ using namespace std;
  * @param vec vector where every second item is erased.
  */
 void eraseEverySecond(std::vector<int>& vec) {
-    auto beg = vec.begin();
-    auto end = vec.end();
-    for (auto i = beg; i != end; i = i+2)
-    { vec.erase(i); }
+    auto it = vec.begin();
+    while (it != vec.end()) {
+        it = vec.erase(it);  // erase the current element and return the iterator to the next element
+        if (it != vec.end()) {
+            ++it;  // skip the next element
+        }
+    }
 }
 
