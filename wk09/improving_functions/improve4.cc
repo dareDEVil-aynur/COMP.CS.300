2754b069b64fb86d739d4fe6af22c65eaba6e10a
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2023-11-04 09:13:15 +0200

    Fix sort

--- wk09/improving_functions/improve4.cc
+++ wk09/improving_functions/improve4.cc
@@ -20,15 +20,31 @@ using RNG = std::default_random_engine;
  * @param end an iterator that points to the end part of the sequence where the quicksort is performed
  * @param rng the random number generator that can be used
  */
-void randomizedThreePartQuicksort(iter begin, iter end, RNG& rng)
-{
-    if (begin == end) return;
-    std::shuffle(begin, end, rng);
-    auto pivot = *(begin + (end - begin)/2);
-    iter middle1 = std::partition(begin, end,
-        [pivot](int val){ return val < pivot; });
-    iter middle2 = std::partition(middle1, end,
-        [pivot](int val){ return !(pivot < val); });
-    randomizedThreePartQuicksort(begin, middle1, rng);
-    randomizedThreePartQuicksort(middle2, end, rng);
+void threeWayPartition(iter begin, iter end, iter& i, iter& j, RNG& rng) {
+    std::uniform_int_distribution<> dis(0, std::distance(begin, end) - 1);
+    std::iter_swap(begin, begin + dis(rng));
+    auto pivot = *begin;
+    i = begin;
+    j = end;
+
+    iter it = begin + 1;
+    while (it < j) {
+        if (*it < pivot) {
+            std::iter_swap(i++, it++);
+        } else if (*it > pivot) {
+            std::iter_swap(--j, it);
+        } else {
+            ++it;
+        }
+    }
+}
+
+void randomizedThreePartQuicksort(iter begin, iter end, RNG& rng) {
+    if (begin >= end) return;
+
+    iter i, j;
+    threeWayPartition(begin, end, i, j, rng);
+
+    randomizedThreePartQuicksort(begin, i, rng);
+    randomizedThreePartQuicksort(j, end, rng);
 }
