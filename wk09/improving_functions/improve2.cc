7844f3424cb5c634f6fe049fa4c299bc6fa39271
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2023-11-04 09:06:44 +0200

    Optimized minValue function

--- wk09/improving_functions/improve2.cc
+++ wk09/improving_functions/improve2.cc
@@ -9,12 +9,10 @@
  * @param vec vector which is searched for the smallest value.
  * @return int - the smallest value or 0 (zero) if vector is empty.
  */
-int minValue(std::vector<int> vec){
-    if ( vec.empty() ) { return 0; }
-    if ( std::is_sorted(vec.begin(), vec.end()) ) {
-        return vec[0];
-    } else {
-        std::sort(vec.begin(), vec.end());
-        return vec[0];
+int minValue(const std::vector<int>& vec){
+    if (vec.empty()) {
+        return 0;
     }
+    
+    return *std::min_element(vec.begin(), vec.end());
 }
